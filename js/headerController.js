1ee2095cf858857d22e8fb34924b926393ff026a
joel lockwood <joeleo3@hotmail.co.uk>
2017-10-10 21:30:40 +0100

    Responsive header with controller added

---
+++ js/headerController.js
@@ -0,0 +1,24 @@
+angular
+    .module('bdSite')
+    .controller('headerController', function($scope) {
+
+
+      $scope.myFunc1 = function() {
+
+      var img = document.getElementById('burger').src;
+
+      if (img.indexOf('navicon.svg')!=-1) {
+          document.getElementById('burger').src  = 'content/images/cancel.svg';
+      }
+      else {
+          document.getElementById('burger').src = 'content/images/navicon.svg';
+      }
+
+      $scope.myFunc2 = function() {
+
+        var toggleOpen = document.getElementById('toggle');
+        toggleOpen.classList.toggle('open');
+
+      }
+    }
+});
