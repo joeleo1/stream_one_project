0da2de17770318f2bb9f6a8917b382914f73a315
Joel Lockwood <joeleo3@hotmail.co.uk>
2017-11-05 15:23:16 +0000

    Tweaked code to suit mobile first

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-angular.module('bdSite', []);
+angular.module('bdSite', ['ngRoute']);
+
+angular.module('bdSite').config(function($locationProvider, $routeProvider) {
+	$locationProvider.html5Mode(true);
+	$routeProvider.when('/', {
+		templateUrl: 'templates/home.html'
+	})
+
+});
