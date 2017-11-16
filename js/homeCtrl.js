318c6693494f545d2432d83490c2b8715b4f844f
Joel Lockwood <joeleo3@hotmail.co.uk>
2017-11-16 00:05:01 +0000

    JS Video adjustments

--- js/homeCtrl.js
+++ js/homeCtrl.js
@@ -6,14 +6,30 @@ angular.module('RouteControllers', [])
   $scope.playPause = function() {
 
   var myVideo = document.getElementById('vid');
+  var hide_overlay = document.getElementById('vid_overlay');
 
-  if (myVideo.paused)
+  if (myVideo.paused) {
       myVideo.play();
+      hide_overlay.style.opacity= "0";
+    }
 
   else {
-      myVideo.pause(); 
+      myVideo.pause();
+      hide_overlay.style.height = "90%";
   }
 
-
 }
+
+  $scope.vidCtrl = function() {
+
+    var vdCon = document.getElementById('vid_overlay');
+
+      if (vdCon.hasAttribute("controls")) {
+          vdCon.removeAttribute("controls")
+
+       } else {
+        vdCon.setAttribute("controls","controls");
+    }
+}
+
 });
